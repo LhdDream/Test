1c45e2afba2169269062691cccd42da260534bb8
LhdDream <626487728@qq.com>
2020-05-31 00:00:00 +0800

    test_github_streak

--- green/green.py
+++ green/green.py
@@ -37,4 +37,4 @@ def daily_commit(start_date, end_date):
 
 
 if __name__ == '__main__':
-    daily_commit(datetime.date(2020, 5, 31), datetime.date(2020, 7, 28))
+    daily_commit(datetime.date(2020, 5, 31), datetime.date(2020, 6, 5))
